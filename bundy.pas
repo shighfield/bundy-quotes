51169b1d921df7e3b179475850304c1daac15122
shighfield <shighfield@users.noreply.github.com>
2017-10-17 08:35:11 -0400

    Add files via upload

---
+++ bundy.pas
@@ -0,0 +1,111 @@
+program Bundy;
+{
+           TieBundy -  CGI  - Version 0.1/CGI
+                            - May 28 2013
+
+       Display a random bundy quote on web page.
+
+        todo:
+
+        Done:
+
+}
+
+// If using FPC enable the below by removing the .  If using VP ignore this.
+{$mode TP}
+uses
+  dos;                          {Disk}
+
+type
+  Foc = File of char;
+
+var
+  tieversion: String;           {Program name and Version}
+  s: String;                    {Generic String}
+  taglist: String;              {Name of the tagline file}
+  Tagcolor: String;             {Colour of the tagline}
+  tag: String;                  {The actual randomized tagline}
+  Ch: Char;
+  Ch2: Char;
+  Tagfile: Foc;
+  FPos: LongInt;
+
+function exedir: string;                         {Get the Main directory}
+var
+  s: string;
+begin
+  s:=paramstr(0);
+  while s[length(s)]<>'/' do dec(s[0]);
+  exedir:=s;
+end;
+
+Procedure ReadConfig;                           {Reads the config information}
+var
+  f: Text;
+begin
+  Assign(F, exedir + 'bundy.cfg');           {tiequote.cfg is in the same dir as the tieread.exe}
+  {$I-}Reset(f);{$I+}
+  if ioresult<>0 then                           {File not found!}
+  begin
+    WriteLn('<HTML>');
+    WriteLn('The Config file does not exist.<BR>');
+    WriteLn('Please place the bundy.cfg in your CGI-BIN Directory.<BR>');
+    WriteLn('<BR> <BR>');
+    WriteLn('<BR>');
+    WriteLn('<BR><BR>');
+    WriteLn('</BODY> </HTML>');
+    Halt;
+  end;
+  ReadLn(F, TagList);                           {Get the datafile name}
+  ReadLn(F, tagcolor);                          {Color you want the tagline in}
+  Close(F);
+end;
+
+
+Procedure _ReadLn(Var F:Foc;Var Line:String);
+var
+  ThisCh,
+  ThatCh: Char;
+Begin
+  Line := '';
+  if FPos = 0 then Line := Ch;
+  Repeat
+    if Eof(F) then Exit;
+    ThatCh := ThisCh;
+    Read(F,ThisCh);
+    If (ThisCh <> #13) AND (ThisCh <> #10) Then Line := Line + ThisCh;
+  Until (ThatCh = #13) And (ThisCh = #10);
+End;
+
+
+Function Newtag(Show: Boolean): String;
+var
+  S: String;
+begin
+  Assign(Tagfile, taglist);
+  Reset(Tagfile);
+  FPos := Random(Filesize(Tagfile));
+  repeat
+    Ch2 := Ch;
+    Dec(FPos);
+    Seek(Tagfile, FPos);
+    Read(Tagfile, Ch);
+  until ((Ch = #13) And (Ch2 = #10)) OR (FPos = 0);
+  _ReadLn(Tagfile, S);
+  Close(Tagfile);
+  Newtag := S;
+end;
+
+
+begin
+  Randomize;                                    {Get some Randomness}
+  TieVersion := '<Font Color="grey">TieBundy v0.1</FONT>';
+  ReadConfig;                                   {Read config file}
+  writeln('Content-type: text/html');           {Send info to browser}
+  writeln;
+  WriteLn(tagcolor);
+  Tag := NewTag(True);
+  WriteLn(Tieversion);
+  WriteLn(' ...' + tag);
+  WriteLn('</FONT>');
+end.
---
+++ readme.txt
@@ -0,0 +1,26 @@
+Tie Bundy v0.1
+--------------
+Written by: Shawn Highfield (shighfield@gmail.com)
+http://www.tinysbbs.com
+
+What is it?
+-----------
+Small program that will biff out a quote from the TV show married with
+children onto your web page.  Your web server should allow Server Side Includes.
+
+Installing
+----------
+1) Compile using FPC (or use included Windows .exe)
+2) Place bundy.exe in your cgi-bin directory
+3) Copy the bundy quotes file into your cgi-bin dir.
+4) Edit bundy.cfg to suit your system. (and copy to cgi-bin dir)
+
+Bug reports
+-----------
+You can fix it yourself as the pascal source is included and it's probably
+the smallest simplest program you can find.  Took me aprox 3 minutes to
+write. :)
+
+Or you can contact me at the gmail address above.
+
+May 28 2013 Shawn Highfield
